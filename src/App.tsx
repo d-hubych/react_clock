8f2376d7f63b92032c47c0bc9f05f16ac33b0439
Dmytro Hubych <dh.hubych@gmail.com>
2022-11-01 12:13:19 +0200

    reset task in new branch 'develop-2.0' for remember this topic

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,39 @@
 import React from 'react';
 import './App.scss';
-import { Clock } from './components/Clock';
 
-type Props = {};
-type State = {
-  isClockVisible: boolean;
-  clockName: number;
-};
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
 
-class App extends React.Component <Props, State> {
-  state: State = {
-    isClockVisible: true,
-    clockName: 0,
-  };
-
-  clockShowON = () => this.setState({ isClockVisible: true });
-
-  clockShowOFF = () => this.setState({ isClockVisible: false });
-
-  changeClockName = () => this.setState({
-    clockName: (Math.floor(Math.random() * 1000)),
-  });
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App__watch">
-          <h1>React watch</h1>
-          { this.state.isClockVisible
-            ? <Clock name={this.state.clockName} />
-            : <p></p> }
-          <div>
-            <button
-              type="button"
-              onClick={this.clockShowON}
-              className="App__button App__button--show"
-            >
-              Show
-            </button>
-
-            <button
-              type="button"
-              onClick={this.clockShowOFF}
-              className="App__button App__button--hide"
-            >
-              Hide
-            </button>
-
-            <button
-              type="button"
-              onClick={this.changeClockName}
-              className="App__button App__button--name"
-            >
-              Name
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  return `Clock-${value}`;
 }
 
-export default App;
+export const App: React.FC = () => {
+  const today = new Date();
+  let clockName = 'Clock-0';
+
+  // This code starts a timer
+  const timerId = window.setInterval(() => {
+    clockName = getRandomName();
+  }, 3300);
+
+  // this code stops the timer
+  window.clearInterval(timerId);
+
+  return (
+    <div className="App">
+      <h1>React clock</h1>
+
+      <div className="Clock">
+        <strong className="Clock__name">
+          {clockName}
+        </strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    </div>
+  );
+};
